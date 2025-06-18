7bca4a7367de8616385c321e73f7fd1f9ef72988
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-06-18 18:24:06 +0500

    videoSection

--- src/components/MakeBetter.jsx
+++ src/components/MakeBetter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import menuBg from '../assets/images/make-better.png';
+import menuBg from '../assets/images/makeBetter.png';
+import { Link } from 'react-router-dom';
 
 const MakeBetter = () => {
   return (
@@ -16,11 +17,12 @@ const MakeBetter = () => {
             LET’S MAKE GAMES BETTER,{' '}
             <span className="text-orange-500">TOGETHER.</span>
           </h2>
-          <button className="relative inline-flex items-center px-6 py-3 text-sm font-medium text-white group w-40 justify-center bg-transparent border-0">
+       <Link to='/contact-us'>   <button className="relative inline-flex items-center px-6 py-3 text-sm font-medium text-white group w-40 justify-center bg-transparent border-0">
             <span className="absolute top-0 left-0 w-6 h-6 border-t-2 border-l-2 border-orange-500 transition-transform duration-300 group-hover:-translate-x-2 group-hover:-translate-y-2 origin-top-left"></span>
             <span className="absolute bottom-0 right-0 w-6 h-6 border-b-2 border-r-2 border-orange-500 transition-transform duration-300 group-hover:translate-y-2 origin-bottom-right font-secondary"></span>
             CONTACT US
           </button>
+          </Link>
         </div>
       </div>
     </section>
